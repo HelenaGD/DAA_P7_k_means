5e1b5b530b62b1e167fd9b597c5ac63f3b1afcee
HelenaGD <alu0100829150@ull.edu.es>
2023-03-23 18:45:33 +0000

    Tests para la distancia euclídea entre dos puntos ok

--- tests/test_problem.cc
+++ tests/test_problem.cc
@@ -9,6 +9,13 @@ TEST(Problem, Constructor) {
   EXPECT_EQ(problem.get_points(), result);
 }
 
+TEST(Problem, euclidean_distance) {
+  Problem<double> problem(3, 2, {{1, 2}, {3, 4}, {5, 6}});
+  EXPECT_EQ(problem.euclidean_distance({1, 2}, {3, 4}), 2.8284271247461903);
+  Problem<int> problem2(3, 2, {{1, 2, 3}, {4, 5, 6}});
+  EXPECT_EQ(problem2.euclidean_distance({1, 2, 3}, {4, 5, 6}), 5.196152422706632);
+}
+
 // Ejecutamos los casos de prueba
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
