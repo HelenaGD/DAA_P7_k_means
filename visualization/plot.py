a8f42ad4e1135646aa8108cc7d856e7dd2299499
HelenaGD <alu0100829150@ull.edu.es>
2023-04-18 10:21:53 +0000

    Visualización actualizada

--- visualization/plot.py
+++ visualization/plot.py
@@ -1,12 +1,13 @@
 # Referencias: https://pythondiario.com/2018/08/graficos-en-tercera-dimension-3d-con.html
-
+# Traerse archivos
+# scp usuario@10.6.128.144:~/DAA/DAA_P7_k_means/solutions/* .
 # Scatter
 
 # importamos las librerias necesarias
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 
-name = "solution.txt"
+name = 'grasp'
 
 # Creamos la figura
 fig = plt.figure()
@@ -14,7 +15,7 @@ fig = plt.figure()
 ax1 = fig.add_subplot(111, projection='3d')
 
 # Lee el archivo de texto
-with open('/solutions/' + name) as f:
+with open(name + '.txt') as f:
     # Lee la primera línea como sse
     sse = float(f.readline().strip())
     # Lee la segunda línea como el número de clusters
@@ -39,22 +40,24 @@ with open('/solutions/' + name) as f:
 
 # Agregamos los puntos en el plano 3D
 colors = ['g', 'b', 'r', 'c', 'm', 'y', 'k'] # Lista de colores para cada cluster
+num_colors = len(colors)
 for i in range(num_clusters):
     # Dibuja los centroides
     centroid = centroids[i]
-    ax1.scatter(centroid[0], centroid[1], centroid[2], c=colors[i], marker='o', s=150)
+    color_index = i % num_colors # Índice del color a usar (cíclico)
+    ax1.scatter(centroid[0], centroid[1], centroid[2], c=colors[color_index], marker='o', s=150)
     # Dibuja los puntos de cada cluster
     cluster_points = clusters[i]
     x = [p[0] for p in cluster_points]
     y = [p[1] for p in cluster_points]
     z = [p[2] for p in cluster_points]
-    ax1.scatter(x, y, z, c=colors[i], marker='o')
+    ax1.scatter(x, y, z, c=colors[color_index], marker='o')
 
 # Agregamos el título con la SSE y el número de clusters
 ax1.set_title('SSE: {} - Número de clusters: {}'.format(sse, num_clusters))
 
 # Guardamos la gráfica en un archivo PNG
-fig.savefig('grafica.png')
+fig.savefig(name + '.png')
 
 # Mostramos el gráfico
 plt.show()
